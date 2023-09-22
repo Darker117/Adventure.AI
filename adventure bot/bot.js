09da74bc2c6cf78effa3ac4f532f280101bb4896
Darker117 <145191474+Darker117@users.noreply.github.com>
2023-09-21 23:19:21 -0500

    Update bot.js

--- adventure bot/bot.js
+++ adventure bot/bot.js
@@ -1,13 +1,10 @@
-// Load environment variables from .env file
 require('dotenv').config();
-
 const { Client, GatewayIntentBits } = require('discord.js');
 const axios = require('axios');
 
 const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
-// Initialize Discord Client
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -15,59 +12,60 @@ const client = new Client({
   ]
 });
 
-// Store the state of each user's adventure
 const userAdventures = new Map();
 
 const registerCommands = async () => {
-    try {
-      const commands = [
-        { 
-          name: 'start-adventure', 
-          description: 'Start a text-based adventure game' 
-        },
-        { 
-          name: 'say', 
-          description: 'Say something in your adventure',
-          options: [{ name: 'message', type: 3, description: 'What you want to say', required: true }] 
-        },
-        { 
-          name: 'do', 
-          description: 'Perform an action in your adventure',
-          options: [{ name: 'action', type: 3, description: 'The action you want to perform', required: true }] 
-        },
-        { 
-          name: 'story', 
-          description: 'Add narrative to your adventure',
-          options: [{ name: 'narrative', type: 3, description: 'The narrative you want to add', required: true }] 
-        }
-      ];
-  
-      // Register the commands globally
-      await client.application?.commands.set(commands);
-    } catch (error) {
-      console.error('Failed to register commands:', error);
-    }
-  };
+  try {
+    const commands = [
+      { 
+        name: 'start-adventure', 
+        description: 'Start a text-based adventure game' 
+      },
+      { 
+        name: 'say', 
+        description: 'Say something in your adventure',
+        options: [{ name: 'message', type: 3, description: 'What you want to say', required: true }] 
+      },
+      { 
+        name: 'do', 
+        description: 'Perform an action in your adventure',
+        options: [{ name: 'action', type: 3, description: 'The action you want to perform', required: true }] 
+      },
+      { 
+        name: 'story', 
+        description: 'Add narrative to your adventure',
+        options: [{ name: 'narrative', type: 3, description: 'The narrative you want to add', required: true }] 
+      },
+      { 
+        name: 'continue', 
+        description: 'Continue your saved adventure' 
+      }
+    ];
+
+    await client.application?.commands.set(commands);
+  } catch (error) {
+    console.error('Failed to register commands:', error);
+  }
+};
 
 const getAdventureResponse = async (userId, message) => {
   const previousMessages = userAdventures.get(userId) || [];
   previousMessages.push({ role: 'user', content: message });
 
   const response = await axios.post(
-    'https://api.openai.com/v1/chat/completions',
+    'https://api.openai.com/v1/engines/davinci-codex/completions',
     {
-      model: 'gpt-3.5-turbo',
-      messages: previousMessages
+      prompt: previousMessages.map(msg => `${msg.role}: ${msg.content}`).join('\n'),
+      max_tokens: 100
     },
     {
       headers: {
-        'Authorization': `Bearer ${OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
+        'Authorization': `Bearer ${OPENAI_API_KEY}`
       }
     }
   );
 
-  const nextMessage = response.data.choices[0]?.message?.content || 'Unknown response';
+  const nextMessage = response.data.choices[0].text.trim();
   previousMessages.push({ role: 'assistant', content: nextMessage });
   userAdventures.set(userId, previousMessages);
   return nextMessage;
@@ -81,16 +79,32 @@ client.once('ready', async () => {
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
   const userId = interaction.user.id;
 
-  await interaction.deferReply();
+  try {
+    await interaction.deferReply();
+  } catch (error) {
+    console.error('Failed to defer reply:', error);
+    return;
+  }
 
   try {
+    const commandName = interaction.commandName;
+
     if (commandName === 'start-adventure') {
-      const startMessage = 'You find yourself in a mysterious forest. What will you do?';
+      const startMessage = 'You wake up in the heart of a quaint little town, surrounded by chatter and the aroma of fresh-baked bread. Nearby, a town crier announces something about a mysterious quest. You cant shake the feeling that destiny is calling. What will you do?';
       userAdventures.set(userId, [{ role: 'system', content: 'You are embarking on a text-based adventure game.' }, { role: 'assistant', content: startMessage }]);
       await interaction.followUp(startMessage);
+    }
+
+    if (commandName === 'continue') {
+      const previousMessages = userAdventures.get(userId);
+      if (previousMessages && previousMessages.length > 0) {
+        const lastMessage = previousMessages[previousMessages.length - 1];
+        await interaction.followUp(`Continuing your adventure: ${lastMessage.content}`);
+      } else {
+        await interaction.followUp("You don't have a saved adventure to continue.");
+      }
     }
 
     if (commandName === 'say') {
@@ -110,7 +124,6 @@ client.on('interactionCreate', async interaction => {
       const nextMessage = await getAdventureResponse(userId, `/story ${userNarrative}`);
       await interaction.followUp(nextMessage);
     }
-
   } catch (error) {
     console.error('An error occurred:', error);
     await interaction.followUp('An error occurred. Please try again later.');
